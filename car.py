05260275d68576d2999e2d8fdca4bc98e233a9fc
Jessica Soler <solerj@wncc.edu>
2024-04-07 10:45:08 -0600

    pseudocode
    pseudocode car_go file and class file

---
+++ car.py
@@ -0,0 +1,13 @@
+"""
+    car.py
+    Jessica Soler
+    4/7/24
+    Car Class for car_go.py file
+"""
+
+#TODO: create a class for car
+#attributes: _color && _speed
+
+#initialize in __init__
+
+#Methods: accelerate() brake() get_speed() get_color()
