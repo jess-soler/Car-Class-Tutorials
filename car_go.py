032d02ca6a0f4e15eab0b0a163b708ddd9dd7720
Jessica Soler <solerj@wncc.edu>
2024-04-07 13:16:37 -0600

    trying to troubleshoot my class
    I can't get it to recognize my class

--- car_go.py
+++ car_go.py
@@ -6,14 +6,15 @@
 """
 import car
 
-my_car = car()
+def main():
+    
+    my_car = car.car()
+    #Class attributes: self, _color, _speed
+    #Class methods: accelerate() brake() get_speed() get_color()
 
-#Class attributes: self, _color, _speed
-#Class methods: accelerate() brake() get_speed() get_color()
-
-#TODO: get input: what color is your car
-car_color = my_car.get_color()
-print(f"The car color is: {car_color}")
+    #TODO: get input: what color is your car
+    car_color = my_car.get_color()
+    print(f"The car color is: {car_color}")
 
 #TODO: output the blue car is going 50 mph
 
@@ -27,3 +28,19 @@ print(f"The car color is: {car_color}")
 #TODO: output: the blue car is going 50 mph
 
 #TODO: get input: (a)ccellerate or (b)rake or (e)xit: e
+#  while True:
+#         choice = input("(a)ccelerate, (b)rake, or (e)xit: ")
+#         if choice == "a":
+#             # TODO: Implement acceleration logic
+#             pass
+#         elif choice == "b":
+#             # TODO: Implement braking logic
+#             pass
+#         elif choice == "e":
+#             print("Exiting the program.")
+#             break
+#         else:
+#             print("Invalid choice. Please select (a), (b), or (e).")
+            
+if __name__ == "__main__":
+    main()
