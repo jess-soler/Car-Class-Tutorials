a201de3c587bb80308a402515be1c9c7b3abd2fa
Jessica Soler <solerj@wncc.edu>
2024-04-07 13:01:51 -0600

    attempting first function in class
    trying to create my_car instance using class Car.  and coding get_color() in car.py but I am getting an error that says module is not callable

---
+++ car_go.py
@@ -0,0 +1,29 @@
+"""
+    car_go.py
+    Jessica Soler
+    4/7/24
+    Car Class for car_go.py file
+"""
+import car
+
+my_car = car()
+
+#Class attributes: self, _color, _speed
+#Class methods: accelerate() brake() get_speed() get_color()
+
+#TODO: get input: what color is your car
+car_color = my_car.get_color()
+print(f"The car color is: {car_color}")
+
+#TODO: output the blue car is going 50 mph
+
+#TODO: get input: (a)ccellerate or (b)rake: a
+#TODO: output: the blue car is going 60 mph
+
+#TODO: get input: (a)ccellerate or (b)rake or (e)xit: b
+#TODO: output: the blue car is going 55 mph
+
+#TODO: get input: (a)ccellerate or (b)rake or (e)xit: b
+#TODO: output: the blue car is going 50 mph
+
+#TODO: get input: (a)ccellerate or (b)rake or (e)xit: e
